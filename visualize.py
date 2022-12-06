9e9ae7009ae5e3ec0e630643d73dc2680198d0b3
Yelan Tao <yeltao@ethz.ch>
2022-12-06 19:49:45 +0100

    move jupytor notebook to py and add visualization

---
+++ visualize.py
@@ -0,0 +1,48 @@
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+import open3d as o3d
+from models.cvae import *
+from preprocess.preprocess_optimize import *
+from preprocess.bps_encoding import *
+from utils import *
+from utils_read_data import *
+
+prox_dataset_path = '/local/home/yeltao/Desktop/3DHuman_gen/dataset/proxe'
+scene_name = 'N3OpenArea'
+
+# read scen mesh/sdf
+scene_mesh, cur_scene_verts, s_grid_min_batch, s_grid_max_batch, s_sdf_batch = read_mesh_sdf(prox_dataset_path,
+                                                                                             'prox',
+                                                                                             scene_name)
+# draw box
+mesh_box_1 = o3d.geometry.TriangleMesh.create_box(width=3.5, height=0.1, depth=2.5)
+mesh_box_2 = o3d.geometry.TriangleMesh.create_box(width=3.5, height=0.1, depth=2.5)
+mesh_box_3 = o3d.geometry.TriangleMesh.create_box(width=0.1, height=3.6, depth=2.5)
+mesh_box_4 = o3d.geometry.TriangleMesh.create_box(width=0.1, height=3.6, depth=2.5)
+mesh_box_1.paint_uniform_color([0.9, 0.1, 0.1]) # red
+mesh_box_2.paint_uniform_color([0.1, 0.9, 0.1]) # green
+mesh_box_3.paint_uniform_color([0.1, 0.1, 0.9]) # blue
+mesh_box_4.paint_uniform_color([0.9, 0.9, 0.1]) # yellow
+
+scene_center = scene_mesh.get_center()
+box_center_1 = mesh_box_1.get_center()
+box_center_2 = mesh_box_3.get_center()
+mesh_box_1.translate((scene_center[0] - box_center_1[0], box_center_1[1] - 2.4, -1))
+mesh_box_2.translate((scene_center[0] - box_center_1[0], box_center_1[1] + 1.2, -1))
+mesh_box_3.translate((-box_center_2[0] + 2.1, scene_center[1] - box_center_2[1], -1))
+mesh_box_4.translate((-box_center_2[0] - 1.4, scene_center[1] - box_center_2[1], -1))
+
+R = scene_mesh.get_rotation_matrix_from_xyz((0, 0, 0.1))
+mesh_box_1.rotate(R, center=(0,0,0))
+mesh_box_2.rotate(R, center=(0,0,0))
+mesh_box_3.rotate(R, center=(0,0,0))
+mesh_box_4.rotate(R, center=(0,0,0))
+humans = []
+for i in range(10):
+    body = o3d.io.read_triangle_mesh('human_meshes/' + str(i) + '.ply')
+    body.compute_vertex_normals()
+    humans.append(body)
+
+# use normal open3d visualization
+o3d.visualization.draw_geometries([mesh_box_1, mesh_box_2, mesh_box_3, mesh_box_4, scene_mesh] + humans)
