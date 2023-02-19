b69dc202f0d7aaee5f539e1742f545879835677c
Yelan Tao <yeltao@ethz.ch>
2023-02-19 22:50:48 +0100

    custom name

--- mask_ply.py
+++ mask_ply.py
@@ -1,25 +1,50 @@
 import numpy as np
 import open3d as o3d
+import argparse
+import os
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n', type=str, default="N3OpenArea")
+args = parser.parse_args()
+
+directory_pattern =  r'.*{}$'.format(args.n)
+
+pcd = o3d.io.read_point_cloud('/local/home/yeltao/Desktop/3DHuman_gen/dataset/proxe/scenes/'+args.n+'.ply')
+pcd_in_np = np.asarray(pcd.points)
+
+num_files = 0
+
+for dirpath, dirnames, filenames in os.walk('/local/home/yeltao/Downloads'):
+    for dirname in dirnames:
+        if re.match(directory_pattern, dirname):
+            directory_path = os.path.join(dirpath, dirname, "pred_mask")
+            for filename in os.listdir(directory_path):
+                num_files += 1
 
 def processMask(i):
-    pcd = o3d.io.read_point_cloud('/local/home/yeltao/Desktop/3DHuman_gen/dataset/proxe/scenes/N3OpenArea.ply')
-    pcd_in_np = np.asarray(pcd.points)
     mask = []
-    f = open('/local/home/yeltao/Downloads/20230118_173140_N3OpenArea/pred_mask/20230118_173140_N3OpenArea_' + str(i) + '.txt', "r")
-    for line in f:
-        mask.append(int(line.strip('\n')))
+    for dirpath, dirnames, filenames in os.walk('/local/home/yeltao/Downloads'):
+        filename_pattern = r".*_{}\.txt$".format(i)
+        for dirname in dirnames:
+            if re.match(directory_pattern, dirname):
+                directory_path = os.path.join(dirpath, dirname, "pred_mask")
+                for filename in os.listdir(directory_path):
+                    if re.match(filename_pattern, filename):
+                        file_path = os.path.join(directory_path, filename)
+                        with open(file_path, 'r') as f:
+                            for line in f:
+                                mask.append(int(line.strip('\n')))
 
     pcd_in_np_masked = [pcd_in_np[i] for i, v in enumerate(mask) if v == 1]
     pcd_masked = o3d.geometry.PointCloud()
     pcd_masked.points = o3d.utility.Vector3dVector(pcd_in_np_masked)
-    o3d.io.write_point_cloud("scene_meshes/N3OpenArea_" + str(i) + ".ply", pcd_masked, write_ascii=True)
+    o3d.io.write_point_cloud("scene_meshes/"+args.n+"_" + str(i) + ".ply", pcd_masked, write_ascii=True)
     
-
-for i in range(20):
+for i in range(num_files):
     processMask(i)
 
-for j in range(8, 20):
-    pcd = o3d.io.read_point_cloud("scene_meshes/N3OpenArea_" + str(j) + ".ply")
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, 0.1)
-    o3d.io.write_triangle_mesh("scene_meshes/N3OpenArea_"+str(j)+".obj", mesh, write_triangle_uvs=True)
-    o3d.visualization.draw_geometries([mesh])
+# for j in range(num_files):
+#     pcd = o3d.io.read_point_cloud("scene_meshes/"+args.n+"_" + str(j) + ".ply")
+#     mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, 0.1)
+#     o3d.io.write_triangle_mesh("scene_meshes/"+args.n+"_"+str(j)+".obj", mesh, write_triangle_uvs=True)
