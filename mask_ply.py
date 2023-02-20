73c19adb293d82756426be8c13b0cde6b0c1d18b
Yelan Tao <yeltao@ethz.ch>
2023-02-20 15:42:31 +0100

    generate mesh

--- mask_ply.py
+++ mask_ply.py
@@ -44,14 +44,26 @@ def processMask(i):
     pcd_masked = o3d.geometry.PointCloud()
     pcd_masked.points = o3d.utility.Vector3dVector(pcd_in_np_masked)
     o3d.io.write_point_cloud(directory + "/"+args.scene+"_" + str(i) + ".ply", pcd_masked, write_ascii=True)
-    # mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd_masked, 0.1)
-    # o3d.io.write_triangle_mesh(directory + "/"+args.scene+"_"+str(j)+".obj", mesh, write_triangle_uvs=True)
+    
     
 for i in range(num_files):
     processMask(i)
 
 vis = []
 for j in range(num_files):
-    pcd = o3d.io.read_point_cloud(directory + "/" +args.scene+"_" + str(j) + ".ply")
-    vis.append(pcd)
+    pcd_mesh = o3d.io.read_point_cloud(directory + "/" +args.scene+"_" + str(j) + ".ply")
+    
+    # Estimate normal vectors
+    pcd_mesh.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+    # Create mesh from point cloud
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+        pcd_mesh,
+        depth=10,
+        scale=1.1,
+        linear_fit=False)[0]
+
+    o3d.io.write_triangle_mesh(directory + "/"+args.scene+"_"+str(j)+".obj", mesh)
+    mesh_vis = o3d.io.read_triangle_mesh(directory + "/"+args.scene+"_"+str(j)+".obj")
+    vis.append(mesh_vis)
 o3d.visualization.draw_geometries(vis)
